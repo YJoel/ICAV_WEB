96b0ec6dc965175a5bd48e32b6951c4a558b6f23
YJoel <joelgarcia6601@gmail.com>
2025-05-26 04:52:44 -0500

    Se terminó de configurar la aplicacion para agregar, y editar lideres

--- app/icav/dashboard/secretaria/lideres/index.js
+++ app/icav/dashboard/secretaria/lideres/index.js
@@ -1,7 +1,166 @@
-document.addEventListener('load', )
+const formEditarLider = document.forms["formEditarLider"];
+formEditarLider.addEventListener("submit", handlerFormEditarLider);
 
-const tableMiembros = document.getElementById("miembros")
+document.addEventListener("load", mostrarMiembrosHTML());
 
-async function agregarMiembrosHTML () {
-  // const data = await getMiembros();
+const tableMiembros = document.getElementById("lideres");
+const tableAgregarLider = document.getElementById("tableAgregarLider");
+
+async function handlerFormEditarLider(ev) {
+  ev.preventDefault();
+
+  const lider = {
+    id: 0,
+    idMiembro: formEditarLider["idLider"].value,
+    idRol: formEditarLider["idRol"].value,
+    idMinisterio: formEditarLider["ministerios"].value,
+  };
+
+  const data = await updateLider(lider);
+  if (!data.error) {
+    alert("Miembro actualizado con éxito");
+    mostrarMiembrosHTML(); // Volver a cargar la lista de miembros
+    document.getElementById("cerrarModal").click();
+  } else {
+    alert("Error eliminando miembro:", data.error);
+  }
 }
+
+async function mostrarMiembrosHTML() {
+  const data = await getLideres();
+  localStorage.setItem("lideres", JSON.stringify(data));
+
+  tableMiembros.innerHTML = "";
+
+  data.forEach((miembro, i) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${i + 1}</td>
+      <td>${miembro.nombres}</td>
+      <td>${miembro.apellidos}</td>
+      <td>${miembro.nombre}</td>
+      <td>
+        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editarLider" onclick="editarLider(${
+          miembro.identificacion
+        })">Editar</button>
+        <button class="btn btn-danger" onclick="eliminarLider(${
+          miembro.identificacion
+        })">Eliminar</button>
+      </td>
+    `;
+    tableMiembros.appendChild(row);
+  });
+}
+
+async function editarLider(id) {
+  await cargarMinisterios("ministerios");
+
+  const lideres = JSON.parse(localStorage.getItem("lideres"));
+  const lider = lideres.find((lider) => lider.idMiembro == id);
+
+  formEditarLider["nombres"].value = lider.nombres;
+  formEditarLider["apellidos"].value = lider.apellidos;
+  formEditarLider["ministerios"].value = lider.idMinisterio;
+  formEditarLider["idLider"].value = lider.idMiembro;
+  formEditarLider["idRol"].value = lider.idRol;
+}
+
+async function cargarMinisterios(id) {
+  const ministeriosEL = document.getElementById(id);
+  if (!ministeriosEL.children.length) {
+    const data = await getMinisterios();
+    data.forEach((ministerio) => {
+      const option = document.createElement("option");
+      option.value = ministerio.id;
+      option.innerHTML = ministerio.nombre;
+      ministeriosEL.appendChild(option);
+    });
+  }
+}
+
+async function mostrarMiembrosEnAgregarLider() {
+  const jsonMiembros = localStorage.getItem("miembros");
+  let miembros = [];
+  if (jsonMiembros != null) {
+    miembros = JSON.parse(localStorage.getItem("miembros"));
+  } else {
+    miembros = await getMiembros();
+    localStorage.setItem("miembros", JSON.stringify(miembros));
+  }
+
+  tableAgregarLider.innerHTML = "";
+
+  miembros.forEach((miembro, i) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${i + 1}</td>
+      <td>${miembro.nombres}</td>
+      <td>${miembro.apellidos}</td>
+      <td>
+        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalAsignarMinisterio" onclick="mostratModalMinisterio(${
+          miembro.identificacion
+        })">Asignar Ministerio</button>
+      </td>
+    `;
+    tableAgregarLider.appendChild(row);
+  });
+}
+
+document
+  .getElementById("searchInput")
+  .addEventListener("keyup", filtrarMiembros);
+
+function filtrarMiembros(ev) {
+  const miembros = JSON.parse(localStorage.getItem("miembros"));
+  tableAgregarLider.innerHTML = "";
+
+  let filter = miembros.filter((miembro) => {
+    return (
+      miembro.nombres.toLowerCase().includes(ev.target.value.toLowerCase()) ||
+      miembro.apellidos.toLowerCase().includes(ev.target.value.toLowerCase())
+    );
+  });
+
+  filter.forEach((miembro, i) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${i + 1}</td>
+      <td>${miembro.nombres}</td>
+      <td>${miembro.apellidos}</td>
+      <td>
+        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalAsignarMinisterio" onclick="mostratModalMinisterio(${
+          miembro.identificacion
+        })">Asignar Ministerio</button>
+      </td>
+    `;
+    tableAgregarLider.appendChild(row);
+  });
+}
+
+const formAsignarMinisterio = document.forms["formAsignarMinisterio"];
+function mostratModalMinisterio(id) {
+  const form = document.forms["formAsignarMinisterio"];
+  form["idMiembro"].value = id;
+  cargarMinisterios("ministerioSeleccionado");
+}
+
+formAsignarMinisterio.addEventListener("submit", async (ev) => {
+  ev.preventDefault();
+
+  const lider = {
+    id: 0,
+    idMiembro: formAsignarMinisterio["idMiembro"].value,
+    idRol: formAsignarMinisterio["idRol"].value,
+    idMinisterio: formAsignarMinisterio["ministerioSeleccionado"].value,
+  };
+
+  const data = await crearLider(lider);
+  if (!data.error) {
+    alert("Lider creado con éxito");
+    mostrarMiembrosHTML(); // Volver a cargar la lista de miembros
+    document.getElementById("cerrarModalAsignarMinisterio").click();
+    document.getElementById("cerrarModalAgregarLider").click();
+  } else {
+    alert("Error creando lider:", data.error);
+  }
+});
--- app/icav/dashboard/secretaria/lideres/index.php
+++ app/icav/dashboard/secretaria/lideres/index.php
@@ -437,13 +437,14 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/app/icav/dashboard/functions.php');
             <div class="col-4">
               <!-- Button trigger modal -->
               <button type="button" class="btn btn-primary mt-3 mb-3" data-bs-toggle="modal"
-                data-bs-target="#modalAgregarMiembro" onclick="dataToForm(null, 1)">
+                data-bs-target="#modalAgregarLider" onclick="mostrarMiembrosEnAgregarLider()">
                 Agregar Lider
               </button>
             </div>
           </div>
+          <hr>
           <div class="row">
-            <div class="col-3">
+            <div class="col">
               <div class="h2 text-center">
                 LIDERES
               </div>
@@ -458,12 +459,136 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/app/icav/dashboard/functions.php');
                   <th>
                     Apellidos
                   </th>
+                  <th>
+                    Ministerio
+                  </th>
+                  <th>
+                    Acciones
+                  </th>
                 </thead>
-                <tbody id="miembros">
+                <tbody id="lideres">
                 </tbody>
               </table>
             </div>
-            <div class="col-9">
+          </div>
+
+          <!-- MODAL EDITAR LIDER -->
+          <div class="modal fade" id="editarLider" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Editar Lider</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <form id="formEditarLider">
+                    <div class="row mb-2">
+                      <div class="col">
+                        <input class="form-control" placeholder="Nombres" type="text" name="nombres" readonly>
+                      </div>
+                      <div class="col">
+                        <input class="form-control" placeholder="Apellidos" type="text" name="apellidos" readonly>
+                      </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                      <div class="col">
+                        <label for="ministerios" class="form-label">Seleccione el Ministerio</label>
+                        <select class="form-select" name="ministerios" id="ministerios"></select>
+                      </div>
+                    </div>
+
+                    <input type="hidden" name="idLider" value="">
+                    <input type="hidden" name="idRol" value="">
+
+                    <input type="submit" value="" id="buttonEditarLider" style="display: none;">
+                  </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                    id="cerrarModal">Cancelar</button>
+                  <button type="button" class="btn btn-primary"
+                    onclick="document.getElementById('buttonEditarLider').click()">Listo</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- MODAL AGREGAR LIDER -->
+          <div class="modal fade" id="modalAgregarLider" tabindex="-1" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog  modal-xl">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Agregar Nuevo Lider</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                    id="cerrarModalAgregarLider"></button>
+                </div>
+                <div class="modal-body">
+                  <div class="row">
+                    <div class="col">
+                      <form id="formAgregarLider">
+                        <input type="search" class="form-control" placeholder="Buscar Miembro" id="searchInput">
+                      </form>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <table class="table table-bordered table-hover table-striped ">
+                        <thead class="table-light">
+                          <th>
+                            #
+                          </th>
+                          <th>
+                            Nombres
+                          </th>
+                          <th>
+                            Apellidos
+                          </th>
+                          <th>
+                            Acciones
+                          </th>
+                        </thead>
+                        <tbody id="tableAgregarLider">
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- MODAL ASIGNAR MINISTERIO -->
+          <div class="modal fade" id="modalAsignarMinisterio" tabindex="-1" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Asignar Ministerio</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <form id="formAsignarMinisterio">
+                    <select name="ministerioSeleccionado" class="form-select" id="ministerioSeleccionado"
+                      onclick="cargarMinisterios(this.id)"></select>
+                    <input type="hidden" name="idMiembro" value="">
+                    <input type="hidden" name="idRol" value="3">
+                    <input type="submit" value="" id="buttonAsignarMinisterio" style="display: none;">
+                  </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                    id="cerrarModalAsignarMinisterio">Cancelar</button>
+                  <button type="button" class="btn btn-primary"
+                    onclick="document.getElementById('buttonAsignarMinisterio').submit()">Asignar Ministerio</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col">
               <div class="ministerios">
                 <div class="ministerio dropend">
                   <a class="card rounded btn border-0 p-2 rounded" style="width: 12rem;" data-bs-toggle="dropdown"
@@ -554,7 +679,6 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/app/icav/dashboard/functions.php');
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
@@ -609,13 +733,11 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/app/icav/dashboard/functions.php');
   <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
     integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
     crossorigin="anonymous"></script> -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-    crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
     integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
     crossorigin="anonymous"></script>
-
+  <script src="ministerios.js"></script>
+  <script src="lideres.js"></script>
   <script src="./../miembros/miembros.js"></script>
   <script src="index.js"></script>
 </body>
